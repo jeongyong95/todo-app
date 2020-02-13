26bb90e64f470fadfca3f95fc3bcc7085a9d29d1
jeongyong95 <jeongyong95@gmail.com>
2020-02-13 18:19:28 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef, useCallback } from 'react';
+import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  //고유값으로 사용될 id 선언
+  //ref를 사용하여 변수담기
+  const nextID = useRef(4);
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextID.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextID.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}></TodoInsert>
+      <TodoList
+        todos={todos}
+        onRemove={onRemove}
+        onToggle={onToggle}
+      ></TodoList>
+    </TodoTemplate>
+  );
+};
+
+export default App;
